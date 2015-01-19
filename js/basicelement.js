db1d457e6b20aa8a13a8f09be6335279a2d9a527
Jeny Elkind <elkind@gmail.com>
2015-01-19 16:27:33 +0200

    fix

--- js/basicelement.js
+++ js/basicelement.js
@@ -18,7 +18,8 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - $(".navbar").height()
+
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
