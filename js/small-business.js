35d40799b2d42a22303871be580f2fc8f031b57c
Jeny Elkind <elkind@gmail.com>
2015-01-20 20:37:55 +0200

    fix

--- js/small-business.js
+++ js/small-business.js
@@ -1,5 +1,5 @@
 /*!
- * Clean Blog v1.0.0 (http://codedesign.elkind.net)
+ * Small Business v1.0.0 (http://codedesign.elkind.net)
  * Copyright 2014 Start Bootstrap
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
